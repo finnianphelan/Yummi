65b40c58de18003cb584a0fbc0d646192169c3d4
finnianphelan <phelan.f@etoncollege.org.uk>
2024-01-23 10:55:53 +0000

    Initial Commit

---
+++ Yummi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummi
+//
+//  Created by Phelan, Finnian (PAH) on 23/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Phelan, Finnian (PAH) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
